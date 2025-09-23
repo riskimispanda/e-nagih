111cae4481d11731a4ba81d5bf245743f3b7b133
riskimispanda <riski.22@students.amikom.ac.id>
2025-09-23 13:49:57 +0700

    ete

--- app/Http/Controllers/ExportControllers.php
+++ app/Http/Controllers/ExportControllers.php
@@ -11,26 +11,41 @@ class ExportControllers extends Controller
 {
     public function exportSemua()
     {
+        activity('Export')
+            ->causedBy(auth()->user()->id)
+            ->log(auth()->user()->name . ' Melakukan Export Semua data pelanggan');
         return Excel::download(new ExportPelanggan, 'semua-pelanggan.xlsx');
     }
 
     public function exportAktif()
     {
+        activity('Export')
+            ->causedBy(auth()->user()->id)
+            ->log(auth()->user()->name . ' Melakukan Export data pelanggan Aktif');
         return Excel::download(new ExportPelanggan('aktif'), 'pelanggan-aktif.xlsx');
     }
 
     public function exportNonAktif()
     {
+        activity('Export')
+            ->causedBy(auth()->user()->id)
+            ->log(auth()->user()->name . ' Melakukan Export data pelanggan Non Aktif');
         return Excel::download(new ExportPelanggan('nonaktif'), 'pelanggan-nonaktif.xlsx');
     }
 
     public function exportPaket($id)
     {
+        activity('Export')
+            ->causedBy(auth()->user()->id)
+            ->log(auth()->user()->name . ' Melakukan Export data pelanggan berdasarkan Paket');
         return Excel::download(new ExportPelanggan('paket', $id), 'pelanggan-paket-'.$id.'.xlsx');
     }
 
     public function exportRingkasanPaket()
     {
+        activity('Export')
+            ->causedBy(auth()->user()->id)
+            ->log(auth()->user()->name . ' Melakukan Export Ringkasan Paket');
         return Excel::download(new ExportPelanggan('ringkasan'), 'ringkasan-per-paket.xlsx');
     }
 }
