a89d7a0f08b3c93c6c5c009b061ce24c453fb212
riskimispanda <riski.22@students.amikom.ac.id>
2025-10-01 01:58:15 +0700

    Fix console

--- routes/console.php
+++ routes/console.php
@@ -7,6 +7,6 @@ use Illuminate\Support\Facades\Schedule;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
-Schedule::command('app:cek-payment')->everyMinute();
-Schedule::command('app:generate-invoice')->everyMinute();
-Schedule::command('app:send-warning')->dailyAt('08:00');
+// Schedule::command('app:cek-payment')->everyMinute();
+// Schedule::command('app:generate-invoice')->everyMinute();
+// Schedule::command('app:send-warning')->dailyAt('08:00');
