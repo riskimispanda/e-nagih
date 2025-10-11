8eeb9bd75069dd99ef69e51606fa622c0da2c68b
riskimispanda <riski.22@students.amikom.ac.id>
2025-10-11 14:55:59 +0700

    mmmm

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,16 +17,10 @@ class Kernel extends ConsoleKernel
     {
         // Jalankan pengecekan pembayaran setiap hari pada pukul 00:05 WIB (GMT+7)
         $schedule->command('app:cek-payment')
-            ->dailyAt('00:05')
+            ->monthlyOn(10, '00:01')
             ->timezone('Asia/Jakarta')
             ->appendOutputTo(storage_path('logs/cek-payment.log'));
-            
-        // Jalankan pengecekan pembayaran dengan mode force setiap minggu pada hari Senin pukul 08:00 WIB
-        $schedule->command('app:cek-payment --force')
-            ->weeklyOn(1, '08:00') // 1 = Senin
-            ->timezone('Asia/Jakarta')
-            ->appendOutputTo(storage_path('logs/cek-payment-force.log'));
-            
+
         $schedule->command('app:test-command')->everyMinute();
         $schedule->command('app:generate-invoice')->everyMinute();
         $schedule->command('app:send-warning')->dailyAt('08:00');
