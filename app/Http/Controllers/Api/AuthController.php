26083ac908a4a09eff07349ff3311252e2e7ac15
riskimispanda <riski.22@students.amikom.ac.id>
2025-12-11 11:12:21 +0700

    Update

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -48,7 +48,7 @@ class AuthController extends Controller
 
         activity('NbillingApps')
             ->causedBy(auth()->user())
-            ->log($user->name . ' Login ke aplikasi');
+            ->log(auth()->user()->name . ' Login ke aplikasi');
 
         return response()->json([
             'access_token' => $token,
