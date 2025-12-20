b2e42f2d7783045948b1c2afc543c28494caccb6
riskimispanda <riski.22@students.amikom.ac.id>
2025-12-21 00:06:51 +0700

    Update Counting Fasum

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -277,7 +277,7 @@ class DataController extends Controller
             })
             ->count();
 
-        $fasum = Customer::where('paket_id', 11)->whereNull('deleted_at')->count();
+        $fasum = Customer::where('paket_id', 11)->where('status_id', 3)->whereNull('deleted_at')->count();
 
         return view('data.data-pelanggan', [
             'users' => auth()->user(),
