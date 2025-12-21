c886f23e7f39a19fea8033c7b660bd2808703d70
riskimispanda <riski.22@students.amikom.ac.id>
2025-12-21 13:07:34 +0700

    Coba

--- app/Http/Controllers/Api/DataControllerApi.php
+++ app/Http/Controllers/Api/DataControllerApi.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+use DB;
 use App\Models\Invoice;
 use App\Models\Pembayaran;
 use Carbon\Carbon;
