c6f2462e2df46c8646474e680ec8498df0720314
riskimispanda <riski.22@students.amikom.ac.id>
2026-05-22 10:35:50 +0700

    update view corporate

--- app/Http/Controllers/PelangganController.php
+++ app/Http/Controllers/PelangganController.php
@@ -8,30 +8,32 @@ use DB;
 // Models
 use App\Models\Customer;
 use App\Models\TiketOpen;
+use App\Models\User;
 
 class PelangganController extends Controller
 {
-    public function corporate()
-    {
-      return view('/data/data-corporate',[
-        'users' => auth()->user(),
-        'roles' => auth()->user()->roles
-      ]);
-    }
+  public function corporate()
+  {
+    return view('/data/data-corporate', [
+      'users' => auth()->user(),
+      'roles' => auth()->user()->roles,
+      'teknisi' => User::where('roles_id', 5)->get()
+    ]);
+  }
 
 
 
-    public function pelangganDismantle()
-    {
-      $pelanggan = Customer::with(['paket', 'tiket', 'tiket.teknisi'])
-          ->onlyTrashed()
-          ->orderBy('updated_at', 'desc')
-          ->get();
+  public function pelangganDismantle()
+  {
+    $pelanggan = Customer::with(['paket', 'tiket', 'tiket.teknisi'])
+      ->onlyTrashed()
+      ->orderBy('updated_at', 'desc')
+      ->get();
 
-      return view('/pelanggan/pelanggan-dismantle',[
-        'users' => auth()->user(),
-        'roles' => auth()->user()->roles,
-        'pelanggan' => $pelanggan
-      ]);
-    }
+    return view('/pelanggan/pelanggan-dismantle', [
+      'users' => auth()->user(),
+      'roles' => auth()->user()->roles,
+      'pelanggan' => $pelanggan
+    ]);
+  }
 }
