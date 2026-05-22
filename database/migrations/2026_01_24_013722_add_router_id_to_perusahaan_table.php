f37cda4c1ebb6b968b25ad3a578c3cdfef88a5aa
riskimispanda <riski.22@students.amikom.ac.id>
2026-05-22 10:51:46 +0700

    add migration new for corporate table

---
+++ database/migrations/2026_01_24_013722_add_router_id_to_perusahaan_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('perusahaan', function (Blueprint $table) {
+            $table->foreignId('router_id')->nullable()->after('perangkat_id')->constrained('router')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('perusahaan', function (Blueprint $table) {
+            $table->dropForeign(['router_id']);
+            $table->dropColumn('router_id');
+        });
+    }
+};
